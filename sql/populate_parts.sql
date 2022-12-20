475bea441139a024aa1d19464ff5a80359013178
Andre Flor <andreflor96@hotmail.com>
2022-12-20 20:29:05 -0300

    table name changes

--- sql/populate_parts.sql
+++ sql/populate_parts.sql
@@ -1,4 +1,4 @@
-insert into "Part" (name) VALUES ('Front Fog Light');
-insert into "Part" (name) VALUES ('High Beam and Low Beam Headlight');
-insert into "Part" (name) VALUES ('Low Beam Headlight');
-insert into "Part" (name) VALUES ('High Beam Headlight');
+insert into parts (name) VALUES ('Front Fog Light');
+insert into parts (name) VALUES ('High Beam and Low Beam Headlight');
+insert into parts (name) VALUES ('Low Beam Headlight');
+insert into parts (name) VALUES ('High Beam Headlight');
